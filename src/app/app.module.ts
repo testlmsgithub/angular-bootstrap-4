cd92e98ab2a9c398bc06d746ecd6c80743260b94
testlmsgithub <80874471+testlmsgithub@users.noreply.github.com>
2022-10-05 15:12:05 +0530

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { TestGridComponent } from './test-grid/test-grid.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HeaderComponent],
+  // Add Component Name Here!
+  declarations: [AppComponent, HeaderComponent, TestGridComponent],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
